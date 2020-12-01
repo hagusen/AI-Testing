af7b0bb1b03efe005f43c171dbb85b7330165b15
hagusen <john@bit-ab.com>
2020-12-01 22:43:21 +0100

    tested OnOpenAsset

--- Assets/Graph/Runtime/AIContainer.cs
+++ Assets/Graph/Runtime/AIContainer.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using System;
 using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
 
 
 
@@ -12,4 +14,19 @@ public class AIContainer : ScriptableObject
 
 
 
+    // test actually works.... 
+    [OnOpenAssetAttribute(1)]
+    public static bool step1(int instanceID, int line) {
+        string name = EditorUtility.InstanceIDToObject(instanceID).name;
+        Debug.Log("Open Asset step: 1 (" + name + ")");
+        return false; // we did not handle the open
+    }
+
+    // step2 has an attribute with index 2, so will be called after step1
+    [OnOpenAssetAttribute(2)]
+    public static bool step2(int instanceID, int line) {
+        Debug.Log("Open Asset step: 2 (" + instanceID + ")");
+        return false; // we did not handle the open
+    }
+        //
 }
