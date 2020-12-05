b71edb045d17dac9bb6a87cfe0c2f34b28d2aec2
hagusen <john@bit-ab.com>
2020-12-05 21:22:51 +0100

    moved assethandling

--- Assets/Graph/Editor/AIAssetHandler.cs
+++ Assets/Graph/Editor/AIAssetHandler.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Callbacks;
 using UnityEngine;
 
-public class AIAssetHandler : MonoBehaviour
+public class AIAssetHandler
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+#if UNITY_EDITOR // used for all assets...
+    // test actually works.... 
+    [OnOpenAsset(1)]
+    public static bool step1(int instanceID, int line) {
+        var name = EditorUtility.InstanceIDToObject(instanceID);
+        Debug.Log(AssetDatabase.GetAssetPath(instanceID));
+        Debug.Log("Open Asset step: 1 (" + name + ")");
+        return false; // we did not handle the open
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    // step2 has an attribute with index 2, so will be called after step1
+    [OnOpenAsset(1)]
+    public static bool step2(int instanceID, int line) {
+        Debug.Log("Open Asset step: 2 (" + instanceID + ")");
+        return false; // we did not handle the open
     }
+    //
+
+#endif
 }
--- Assets/Graph/Runtime/AIContainer.cs
+++ Assets/Graph/Runtime/AIContainer.cs
@@ -15,22 +15,5 @@ public class AIContainer : ScriptableObject
 
 
 
-#if UNITY_EDITOR // used for all assets...
-    // test actually works.... 
-    [OnOpenAsset(2)]
-    public static bool step1(int instanceID, int line) {
-        string name = EditorUtility.InstanceIDToObject(instanceID).name;
-        Debug.Log("Open Asset step: 1 (" + name + ")");
-        return false; // we did not handle the open
-    }
 
-    // step2 has an attribute with index 2, so will be called after step1
-    [OnOpenAsset(1)]
-    public static bool step2(int instanceID, int line) {
-        Debug.Log("Open Asset step: 2 (" + instanceID + ")");
-        return false; // we did not handle the open
-    }
-        //
-
-#endif
 }
