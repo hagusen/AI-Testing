3050091da8c2b67f45508196bcdcd1a86b2f0a63
hagusen <john@bit-ab.com>
2020-12-08 23:12:28 +0100

    adding attributes

---
+++ Assets/Graph/Editor/BaseNode.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class BaseNode : Node
+{
+
+    public string GUID;
+
+
+    //public bool EntryPoint = false;
+
+
+}
---
+++ Assets/Graph/Editor/CustomAttributes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+
+
+
+
+
+
+public class NodeMenuAttributes : Attribute
+{
+
+}
