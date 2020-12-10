436c267f46c9edb6534d3c564f420e502f0aa7a0
hagusen <john@bit-ab.com>
2020-12-10 23:22:28 +0100

    collection of nodes

---
+++ Assets/Graph/Editor/newNodes/NodeCollector.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public static class NodeCollector
+{
+    static void Test() {
+
+        TypeCache.GetTypesDerivedFrom<BaseNode>();
+
+    }
+
+}
